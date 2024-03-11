3f970b9c4965b77fee8187894ab5427c19833400
Noderyos <vincent.bsod@gmail.com>
2024-03-11 23:36:38 +0100

    Adding bootloader documentation

--- src/boot/main.asm
+++ src/boot/main.asm
@@ -4,15 +4,17 @@ UPPER_MEMORY equ 0x800
 EXTENDED_MEMORY equ 0x802
 KERNEL_LOCATION equ 0x1000
 
+; Saving disk ID
 mov [BOOT_DISK], dl
 
-
+; Setup stack
 xor ax, ax
 mov es, ax
 mov ds, ax
 mov bp, 0x8000
 mov sp, bp
 
+; Retrieve memory size
 xor cx, cx
 xor dx, dx
 mov ax, 0xE801
@@ -20,6 +22,7 @@ int 0x15
 mov word[UPPER_MEMORY], cx
 mov word[EXTENDED_MEMORY], dx
 
+; Reading kernel into disk
 mov bx, KERNEL_LOCATION
 mov dh, 2
 
@@ -31,11 +34,12 @@ mov cl, 0x02
 mov dl, [BOOT_DISK]
 int 0x13
 
+; Setup Video Mode (80x25 16 fore/8 back)
 mov ah, 0x0
 mov al, 0x3
 int 0x10
 
-
+; Switch to protected mode
 CODE_SEG equ GDT_code - GDT_start
 DATA_SEG equ GDT_data - GDT_start
 
@@ -79,6 +83,7 @@ GDT_descriptor:
 
 
 [bits 32]
+; Protected mode entry
 start_protected_mode:
   mov ax, DATA_SEG
   mov ds, ax
@@ -87,9 +92,11 @@ start_protected_mode:
   mov fs, ax
   mov gs, ax
   
-  mov ebp, 0x90000    ; 32 bit stack base pointer
+  ; Setup 32 bit stack base
+  mov ebp, 0x90000 ; address can be used by EBDA, be careful to this in case of a bug
   mov esp, ebp
 
+  ; Run kernel
   jmp KERNEL_LOCATION
 
 times 510-($-$$) db 0

b71fa39d8fad5175bdedc1c8d5e68dc6835688b8
Noderyos <vincent.bsod@gmail.com>
2025-01-12 16:14:41 +0100

    Episode 2

--- boot.asm
+++ boot.asm
@@ -1,25 +1,55 @@
 [bits 16]
 [org 0x7c00]
 
+mov sp, 0x4000
+mov bp, 0x4000
+
+mov ah, 0
+mov al, 0x10
+int 10h
+
 mov si, hello
-print:
+call error
+
+jmp $
+
+error:
+    push si
+    mov bl, 0xc
+    mov di, err
+
+.err_print:
+    mov al, [di]
+    inc di
+ 
+    cmp al, 0
+    je .err_msg_print
+
+    mov ah, 0xe
+    mov bh, 0
+    int 10h
+    jmp .err_print
+
+.err_msg_print:
     mov al, [si]
     inc si
  
     cmp al, 0
-    je end
+    je .err_end
 
     mov ah, 0xe
     mov bh, 0
-    mov bl, 0xf
     int 10h
-    
-    jmp print
-end:
+    jmp .err_msg_print
+.err_end:
+    pop si
+    ret
+
 
 jmp $
 
-hello: db "Hello from NoderyOS", 0
+err: db "ERROR: ", 0
+hello: db "Not really an error btw :)", 13, 10, 0
 
 times 510-($-$$) db 0
 dw 0xaa55

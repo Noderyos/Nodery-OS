efe1665e5bbd08d34dbc57aa4b29380d52df07c5
Noderyos <vincent.bsod@gmail.com>
2025-01-17 11:34:28 +0100

    Episode 4

--- boot.asm
+++ boot.asm
@@ -11,7 +11,7 @@ mov bp, 0x4000
 
 ; Set video mode (640 x 350, 16 colors)
 mov ah, 0
-mov al, 0x10
+mov al, 0x3
 int 10h
 
 ; Read disk
@@ -25,24 +25,69 @@ mov dl, [BOOT_DISK]
 int 13h
 
 ; Check if read error
-jnb 0x7e00
+jb read_error
 
-mov si, disk_error
-call error
-jmp $
+CODE_SEG equ GDT_code - GDT_start
+DATA_SEG equ GDT_data - GDT_start
+
+cli
+lgdt [GDT_descriptor]
+mov eax, cr0
+or eax, 1
+mov cr0, eax
+
+jmp CODE_SEG:start_protected_mode
+
+read_error:
+    mov si, disk_error
+    call error
+    jmp $
 
 %include "print.asm"
 
 disk_error: db "Cannot read disk", 13, 10, 0
 
+GDT_start:
+    GDT_null:
+        dd 0
+        dd 0
+    GDT_code:
+        dw 0xFFFF
+        dw 0x0
+        db 0x0
+        db 0b10011011
+        db 0b11001111
+        db 0x0
+    GDT_data:
+        dw 0xFFFF
+        dw 0
+        db 0
+        db 0b10010011
+        db 0b11001111
+        db 0
+GDT_end:
+
+GDT_descriptor:
+    dw GDT_end - GDT_start - 1
+    dd GDT_start
+
+[bits 32]
+start_protected_mode:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    mov ebp, 0x90000
+    mov esp, ebp
+
+    mov byte[0xb8000], 'N'
+    mov byte[0xb8001], 0x6
+
+    jmp $
+
 times 510-($-$$) db 0
 dw 0xaa55
-
-mov si, hello
-call error
-
-jmp $
-
-hello: db "Not a real error BTW :)", 13, 10, 0
-
-times 1000 db 0
+times 512 db 0
